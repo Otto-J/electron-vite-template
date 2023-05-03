a70235ff948822553a7a6dbf2df0fc088ab6feb6
Otto <fa@ijust.cc>
2023-05-03 21:32:22 +0800

    feat: 自定义打包构建

--- vite-demo/vite.config.ts
+++ vite-demo/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import { devPlugin, getReplacer } from "./plugins/devPlugin";
+import { buildPlugin } from "./plugins/buildPlugin";
 
 import optimizer from "vite-plugin-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), devPlugin(), optimizer(getReplacer())],
+  build: {
+    rollupOptions: {
+      plugins: [buildPlugin()],
+    },
+  },
 });
